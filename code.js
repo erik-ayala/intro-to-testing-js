69a4c774223e07a08ac92e05cf819e21c87ead26
Erik Ayala <ayala.erik61@gmail.com>
2021-04-26 09:49:03 -0500

    added ability to say hello when put in input.

--- code.js
+++ code.js
@@ -4,13 +4,7 @@ function helloWorld() {
 }
 
 function sayHello(input) {
-    if (input === "Alex") {
-        return "Hello Alex!";
-    }else if (input === "Pat") {
-        return "Hello Pat!";
-    } else {
-        return "Hello Jane!";
-    }
+    return ("Hello " + input + "!");
 }
 
 
